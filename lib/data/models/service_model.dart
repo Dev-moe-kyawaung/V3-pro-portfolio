22382771279df4df986bee9d33ed86144be1d67e
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-07-05 21:06:08 +0630

    Add ServiceModel class with JSON serialization

--- lib/data/models/service_model.dart
+++ lib/data/models/service_model.dart
@@ -1,1 +1,22 @@
 // Service data model
+import 'package:json_annotation/json_annotation.dart';
+
+part 'service_model.g.dart';
+
+@JsonSerializable()
+class ServiceModel {
+  final String title;
+  final String description;
+  final List<String> points;
+
+  const ServiceModel({
+    required this.title,
+    required this.description,
+    required this.points,
+  });
+
+  factory ServiceModel.fromJson(Map<String, dynamic> json) =>
+      _$ServiceModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ServiceModelToJson(this);
+}
