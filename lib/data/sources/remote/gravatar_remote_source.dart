890c326047da7d7e435f9a4de4c7748844e06ec5
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-07-05 21:14:43 +0630

    Update gravatar_remote_source.dart

--- lib/data/sources/remote/gravatar_remote_source.dart
+++ lib/data/sources/remote/gravatar_remote_source.dart
@@ -1,1 +1,8 @@
 // Gravatar remote data source
+class GravatarRemoteSource {
+  const GravatarRemoteSource();
+
+  String avatarUrl(String email) {
+    throw UnimplementedError('Add gravatar hashing later if needed.');
+  }
+}
