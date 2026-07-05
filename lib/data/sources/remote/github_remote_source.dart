7fab53d9807cac4e807e57835bea21551d3bb956
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-07-05 21:14:22 +0630

    Update github_remote_source.dart

--- lib/data/sources/remote/github_remote_source.dart
+++ lib/data/sources/remote/github_remote_source.dart
@@ -1,1 +1,8 @@
 // GitHub remote data source
+class GitHubRemoteSource {
+  const GitHubRemoteSource();
+
+  Future<Map<String, dynamic>> getUserProfile(String username) async {
+    throw UnimplementedError('Connect GitHub API here later.');
+  }
+}
